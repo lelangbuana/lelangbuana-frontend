331985b6ea7eb66c9c8ddf4d03e8884eaaa2deeb
gunturkh <guntur.kh@gmail.com>
2018-08-27 11:10:01 +0700

    Add ButtonModern Component

---
+++ src/ButtonModern/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+class ButtonModern extends React.Component {
+  render() {
+    return (
+      <div>
+        <Button color="primary">primary</Button>{' '}
+        <Button color="secondary">secondary</Button>{' '}
+        <Button color="success">success</Button>{' '}
+        <Button color="info">info</Button>{' '}
+        <Button color="warning">warning</Button>{' '}
+        <Button color="danger">danger</Button>{' '}
+        <Button color="link">link</Button>
+      </div>
+    );
+  }
+}
+
+export default ButtonModern
