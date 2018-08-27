15170ddef704f73b56d3beb223da7cbe5523108f
gunturkh <guntur.kh@gmail.com>
2018-08-27 11:38:46 +0700

    Add NavBar Component

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import ButtonModern from '../ButtonModern'
+import NavBar from '../NavBar'
+import Header from '../Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Header/>
+        <NavBar/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header>LelangBuana</header>
+        )
+    }
+}
+export default Header;
