f91bc8bd79d17cd396d8900a2530baf2528a8134
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-31 09:34:30 +0700

    Add Redux DevTools basic store

--- src/App/index.js
+++ src/App/index.js
@@ -119,7 +119,10 @@ const reducer = (state = initialState, action) => {
     }
 }
 
-const store = createStore(reducer)
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 class App extends Component {
     render() {
