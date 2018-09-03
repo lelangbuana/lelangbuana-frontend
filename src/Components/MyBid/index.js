6b60b72de769617b2ef88fdafabb246029f15c6f
gunturkh <guntur.kh@gmail.com>
2018-09-03 22:09:19 +0700

    Create My Bid History page

--- src/Components/MyBid/index.js
+++ src/Components/MyBid/index.js
@@ -1,8 +1,85 @@
 import React, {Component} from 'react'
 import { Table } from 'reactstrap'
+import axios from 'axios'
+
+const request = axios.create({
+    baseURL: 'https://lelangbuana.herokuapp.com' || 'http://localhost:3000',
+    timeout: 10000,
+    headers: { Authorization: '' }
+})
 
 class MyBid extends Component {
+    constructor(props) {
+        super(props)
+        this.createBidsHistories = this.createBidsHistories.bind(this)
+        this.state = {
+            myBids: [],
+            title: ''
+        }
+    }
+
+    componentDidMount(){
+        request
+            .get(`/bids/user_id/${localStorage.getItem('user_id')}`)
+            .then((response) => { return response.data.bidData })
+            .then(data => {
+                data.forEach(item => {
+                    request
+                        .get(`/auctions/${item.auction_id}`)
+                        .then((response) => { 
+                            
+                            return response.data 
+                        })
+                        .then(data => {
+                            request
+                                .get(`/users/id/${data.user_id}`)
+                                .then((response) => {return response.data.user.username})
+                                .then(username => {
+                                    this.setState(prevState => {
+                                        console.log(prevState.myBids)
+                
+                                        return {
+                                            myBids: prevState.myBids.concat({
+                                                bids_nominal: item.bids_nominal,
+                                                created_at: item.created_at,
+                                                auction_id: item.auction_id,
+                                                user_id: item.user_id,
+                                                title: data.title,
+                                                username: username
+                                            })
+                                        }
+                                    })
+                                })
+                                
+                            
+                            console.log('LOG FROM AUCTION: ', this.state.title)
+                        })
+                        .catch(error=>{console.log(error)})
+
+                    
+                })
+            })
+            .catch(error=>{console.log(error)})
+    }
+
+    createBidsHistories(item, index) {
+        return (
+            <tbody key={index}>
+                <tr>
+                    <td>{item.created_at}</td>
+                    <td>{item.created_at}</td>
+                    <td>{item.title}</td>
+                    <td>{item.username}</td>
+                    <td>{item.bids_nominal}</td>
+                    <td>Success</td>
+                </tr>
+            </tbody>
+        )
+    }
+
     render() {
+        console.log('My Bids : ', this.state.myBids)
+        let listBidHistories = this.state.myBids.map(this.createBidsHistories)
         return (
             <div>
                 <Table hover>
@@ -16,16 +93,7 @@ class MyBid extends Component {
                             <th>Status</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        <tr>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                    </tbody>
+                    {listBidHistories}
                 </Table>
             </div>
         )
