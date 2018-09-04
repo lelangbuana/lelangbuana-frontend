1fe411a7fe0d778f8009b856937f07dbc0d15014
gunturkh <guntur.kh@gmail.com>
2018-09-04 09:33:48 +0700

    Fetch data MyAuction and  Profile component

--- src/Components/Profile/index.js
+++ src/Components/Profile/index.js
@@ -1,7 +1,58 @@
 import React, { Component } from 'react'
 import { Media, ListGroup, ListGroupItem, Button } from 'reactstrap'
+import axios from 'axios'
+
+const request = axios.create({
+    baseURL: 'https://lelangbuana.herokuapp.com' || 'http://localhost:3000',
+    timeout: 10000,
+    headers: { Authorization: '' }
+})
 
 class Profile extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            phone_number: '',
+            profile_photo: ''
+
+        }
+    }
+
+    componentDidMount(){
+        request
+            .get(`/users/id/${localStorage.getItem('user_id')}`)
+            .then((response) => {return response})
+            .then(data => {
+                console.log('PROFILE DATA: ',data)
+
+                this.setState({
+                    username: data.data.user.username,
+                    phone_number: data.data.user.phone_number,
+                    profile_photo: data.data.user.profile_photo
+                })
+                
+                // this.setState(prevState => {
+                //     console.log(prevState.myBids)
+                
+                //     return {
+                //         myBids: prevState.myBids.concat({
+                //             bids_nominal: item.bids_nominal,
+                //             created_at: item.created_at,
+                //             auction_id: item.auction_id,
+                //             user_id: item.user_id,
+                //             title: data.title,
+                //             username: username
+                //         })
+                //     }
+                // })
+            })
+            .catch(error=>{console.log(error)})
+                                
+                            
+    }
+
     render(){
         return (
             <ListGroup>
@@ -12,9 +63,9 @@ class Profile extends Component {
                         </Media>
                         <Media body>
                             <Media heading>
-          Username
+                                {this.state.username}
                             </Media>
-        +6281-888-888
+                            {this.state.phone_number}
                         </Media>
                     </Media>
                     
